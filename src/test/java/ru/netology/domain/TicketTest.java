eaa6d5d383ba72d57f6ac5c97c72c9e363074dfb
17Ashbringer76 <alexsharapovwork@gmail.com>
2021-04-29 22:08:53 +0300

    Tests fix

--- src/test/java/ru/netology/domain/TicketTest.java
+++ src/test/java/ru/netology/domain/TicketTest.java
@@ -12,13 +12,16 @@ class TicketTest {
     private Ticket ticket3 = new Ticket(3, 12300, "LED", "KLF", 50);
     private Ticket ticket4 = new Ticket(4, 17000, "LED", "VKO", 150);
     private Ticket ticket5 = new Ticket(5, 15000, "AER", "ARH", 120);
-    private Ticket ticket6 = new Ticket(6, 20000, "LED", "VKO", 60);
-
+    private Ticket ticket6 = new Ticket(6, 20000, "LED", "VKO", 90);
+    private Ticket ticket7 = new Ticket(7, 13000, "AER", "ARH", 110);
+    private Ticket ticket8 = new Ticket(8, 10900, "MMK", "KJA", 130);
+    private Ticket ticket9 = new Ticket(9, 10700, "LED", "VKO", 140);
+    private Ticket ticket10 = new Ticket(10, 12000, "LED", "VKO", 70);
 
     @Test
     public void shouldTicketPriceSort() {
-        Ticket[] expected = new Ticket[]{ticket1, ticket3, ticket2, ticket5, ticket4, ticket6};
-        Ticket[] actual = new Ticket[]{ticket1, ticket2, ticket3, ticket4, ticket5, ticket6};
+        Ticket[] expected = new Ticket[]{ticket1, ticket9, ticket8, ticket10, ticket3, ticket7, ticket2, ticket5, ticket4, ticket6};
+        Ticket[] actual = new Ticket[]{ticket1, ticket2, ticket3, ticket4, ticket5, ticket6, ticket7, ticket8, ticket9, ticket10};
         Arrays.sort(actual);
         assertArrayEquals(expected, actual);
     }
--- src/test/java/ru/netology/manager/TicketManagerTest.java
+++ src/test/java/ru/netology/manager/TicketManagerTest.java
@@ -16,20 +16,29 @@ class TicketManagerTest {
     private Ticket ticket4 = new Ticket(4, 17000, "LED", "VKO", 150);
     private Ticket ticket5 = new Ticket(5, 15000, "AER", "ARH", 120);
     private Ticket ticket6 = new Ticket(6, 20000, "LED", "VKO", 60);
+    private Ticket ticket7 = new Ticket(7, 13000, "AER", "ARH", 110);
+    private Ticket ticket8 = new Ticket(8, 10900, "MMK", "KJA", 130);
+    private Ticket ticket9 = new Ticket(9, 10700, "LED", "VKO", 140);
+    private Ticket ticket10 = new Ticket(10, 12000, "LED", "VKO", 70);
 
     @BeforeEach
     public void setUp() {
         manager.add(ticket1);
-        manager.add(ticket2);
+        manager.add(ticket9);
+        manager.add(ticket8);
+        manager.add(ticket10);
         manager.add(ticket3);
+        manager.add(ticket7);
+        manager.add(ticket2);
         manager.add(ticket5);
         manager.add(ticket4);
         manager.add(ticket6);
+
     }
 
     @Test
     public void shouldTicketsSearchMoreOne() {
-        Ticket[] expected = new Ticket[]{ticket1, ticket4, ticket6};
+        Ticket[] expected = new Ticket[]{ticket1, ticket9, ticket10, ticket4, ticket6,};
         Ticket[] actual = manager.finAll("LED", "VKO");
         assertArrayEquals(expected, actual);
     }
@@ -47,11 +56,5 @@ class TicketManagerTest {
         Ticket[] actual = manager.finAll("LED", "KLF");
         assertArrayEquals(expected, actual);
     }
-    @Test
-    public void shouldTicketSearchAll() {
-        Ticket[] expected = new Ticket[]{ticket1, ticket2, ticket3, ticket4, ticket5, ticket6};
-        Ticket[] actual = new Ticket[]{ticket1, ticket2, ticket3, ticket5, ticket4, ticket6};
-        Arrays.sort(actual);
 
-    }
 }
